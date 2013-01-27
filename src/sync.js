ab2a7198789c9e500019ffbd64210dd5a4a8be4d
Tony Wang <tonylovejava@gmail.com>
2013-01-27 04:53:01 -0500

    finetune

--- src/sync.js
+++ src/sync.js
@@ -12,7 +12,7 @@ var words = [];
 for(var i =0; i< word_folders.length;++i){
 	var word = word_folders[i];
 	var word_contents = fs.readdirSync("words/"+word);
-	var word_obj = {};
+	var word_obj = {word:word};
 	for(var j = 0 ; j < word_contents.length;++j){
 		word_obj[word_contents[j]] = JSON.parse(fs.readFileSync("words/"+word+"/"+word_contents[j]));
 	}
